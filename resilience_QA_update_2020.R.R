4e5f13f7d1955dbc9404273c7c9ceead54edb414
jmw0057 <57103986+jmw0057@users.noreply.github.com>
2020-07-09 16:32:28 -0500

    Update resilience_QA_update_2020.R.R

--- resilience_QA_update_2020.R.R
+++ resilience_QA_update_2020.R.R
@@ -1,6 +1,6 @@
-#this script takes the unique geoplatform id's and matches them straight from the community to the updated master list on github.
-#manual curation will be needed once you obtain which id's are missing form the master list
-#by checking the id in object editor. email for questions: Kane Cook fkcook12@gmail.com
+#This is an update of resilience_QA.R to run on the updated master list
+#This script takes the unique geoplatform id's and matches them straight from the Resilience community to the updated master list (cdi_master_update_2020.json) on Github.
+#Manual curation will be needed once you obtain which id's are missing from the master list by checking the id in object editor.
 
 
 #read in library needed for reading json files
@@ -19,7 +19,6 @@ for(j in 1:length(datasets[[1]])){
 }
 
 #From the cdi_master.json, make a new list of all of the cdi datasets
-#master_list <- fromJSON("https://raw.githubusercontent.com/fkcook/cdi_master/master/cdi_master.json", nullValue = NA) <- older master list version
 master_list <- fromJSON("https://raw.githubusercontent.com/NASA-IMPACT/cdi_master/master/cdi_master_update_2020.json")
 
 #Make a new vector of gp_ids from the master document
@@ -27,15 +26,13 @@ master_id <- c()
 
 #Populate titles of master document into new vector
 for(k in 1:length(master_list)){
-  #master_id[k] <- master_list[[k]]$geoplatform_id <- older master list version
   master_id[k] <- master_list[[k]][[9]]
 }
 
 #creates a new vector where the id's are check for matches. a no match results in NA. NA means it is in resilience community but not in master list.
+# if we want NA to mean it is in master list and not in resilience community, use: unmatched <- match(master_id, dataset_id, nomatch = NA)
 unmatched <- match(dataset_id, master_id, nomatch = NA)
 #if NA, take the position and check using " dataset_id['position #'] " and taking that ID into object editor.
 #take that dataset back into the master list, and edit accordingly by either correcting the master list
 #or correcting which dataset is linked to the resilience portfolio
-
-# if we want NA to mean it is in master list and not in resilience community, use: unmatched <- match(master_id, dataset_id, nomatch = NA)
 
